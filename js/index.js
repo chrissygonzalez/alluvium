b0f468282da68c0487dc52fea3a586654eb62aa4
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-07-26 16:58:44 -0400

    add styling, fuss with buttons

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-var button = document.getElementById("myBtn");
 var powerData;
-var viewSize = 30;
-function currentStart(end, size) { 
-    return end - size;
+var currentView = {
+    viewSize: 30,
+    startIndex: 0,
+    endIndex: 0
 }
+
+var title = document.getElementById("title");
+var btn7 = document.getElementById("btn7");
+var btn30 = document.getElementById("btn30");
+var btn60 = document.getElementById("btn60");
+var btn90 = document.getElementById("btn90");
+var btn120 = document.getElementById("btn120");
+var back = document.getElementById("back");
+var forward = document.getElementById("forward");
 
 var requestURL = "data/power_consumption.json";
 var request = new XMLHttpRequest();
@@ -14,12 +23,102 @@ request.addEventListener("load", transferComplete);
 
 function transferComplete(e){
     powerData = getPowerData(request.response);
-    var start = currentStart(powerData[0].length, viewSize);
-    getView(powerData, viewSize, start);
+    setStart(powerData[0].length);
+    var start = getStart();
+    var size = getSize();
+    getViewData(powerData, size, start);
+    setTitle();
 }
-button.addEventListener("click", function(e) {
-    //getView(powerData, 8, powerData[0].length - 8);
+
+function setStart(end) {
+    currentView.endIndex = end;
+    currentView.startIndex = currentView.endIndex - currentView.viewSize;
+}
+
+function getStart() { 
+    return currentView.startIndex;
+}
+
+function setEnd(action){
+    var currentEnd = getEnd();
+    var size = getSize();
+    if (action > 0) {
+        if (currentEnd + size < powerData.length) {
+            currentView.endIndex = currentEnd + size;
+        } else {
+            currentView.endIndex = powerData[0].length;
+        }
+    } else if (action < 0) {
+        if (currentEnd - size > 0) {
+            currentView.endIndex = currentEnd - size;
+        } else {
+            currentView.endIndex = size;
+        }
+    }
+}
+
+function getEnd(){
+    return currentView.endIndex;
+}
+
+function setSize(size) { 
+    currentView.viewSize = size;
+}
+
+function getSize() { 
+    return currentView.viewSize;
+}
+
+btn7.addEventListener("click", function(e) {
+    setSize(7);
+    changeView();
 });
+
+btn30.addEventListener("click", function(e) {
+    setSize(30);
+    changeView();
+});
+
+btn60.addEventListener("click", function(e) {
+    setSize(60);
+    changeView();
+});
+
+btn90.addEventListener("click", function(e) {
+    setSize(90);
+    changeView();
+});
+
+btn120.addEventListener("click", function(e) {
+    setSize(120);
+    changeView();
+});
+
+back.addEventListener("click", function(e) {
+    setEnd(-1);
+    changeView();
+});
+
+forward.addEventListener("click", function(e) {
+    setEnd(1);
+    changeView();
+});
+
+function changeView() {
+    setStart(getEnd());
+    var size = getSize();
+    var start = getStart();
+    getViewData(powerData, size, start);
+    setTitle();
+}
+
+function setTitle(){
+    var start = getStart();
+    var end = getEnd();
+    var startDate = new Date(powerData[0][start]);
+    var endDate = new Date(powerData[0][end - 1]);
+    title.innerHTML = startDate.toDateString().slice(4) + " – " + endDate.toDateString().slice(4);
+}
 
 function getPowerData(response) {
     var data = response;
@@ -43,11 +142,7 @@ function getPowerData(response) {
     return [dates, obs, fore, sub1, sub2, sub3, volt];
 }
 
-function getStartDate(data) {
-    
-}
-
-function getView(data, size, begin) {
+function getViewData(data, size, begin) {
     var viewData = [[], [], [], [], [], [], []];
     for (var i = 0; i < data.length; i++) {
         for (var j = begin; j < begin + size; j++) {
@@ -55,25 +150,36 @@ function getView(data, size, begin) {
         }
     }
     drawGraphs(viewData);
-    //return viewData;
 }
 
 function drawGraphs(data){
     var observed = {
         x: data[0],
         y: data[1],
-        marker: {         // marker is an object, valid marker keys: #scatter-marker
-                color: 'rgb(16, 32, 77)' // more about "marker.color": #scatter-marker-color
-            },
+        xaxis: 'x',
         name: "Observed",
-        type: 'scatter'
+        type: 'scatter',
+        line: {
+            shape: "spline",
+            smoothing: 0.5,
+            width: 8,
+            color: "#8FCAC4"
+        }
       };
 
     var forecast = {
         x: data[0],
         y: data[2],
+        xaxis: 'x',
         name: "Forecast",
-        type: 'scatter'
+        type: 'scatter',
+        line: {
+            shape: "spline",
+            smoothing: 0.5,
+            width: 3,
+            dash: "dot",
+            color: "#267D75"
+        }
       };
 
     var voltage = {
@@ -82,17 +188,64 @@ function drawGraphs(data){
         xaxis: 'x2',
         yaxis: 'y2',
         name: "Voltage",
-        type: 'scatter'
+        type: 'scatter',
+        line: {
+            shape: "spline",
+            smoothing: 0.5,
+            width: 8,
+            color: "#99CA8F"
+        }
       };
 
     var consumption = [observed, forecast, voltage];
     
     var topLayout = {
-        yaxis2: {domain: [0, .2]},
-        xaxis2: {anchor: 'y2'},
-        yaxis: {domain: [.3, 1]},
+        title: "Title",
+        paper_bgcolor: "#252831",
+        plot_bgcolor: "#252831",
+        yaxis2: {
+            domain: [0, .2],
+            tickfont: {
+                family: "Overpass",
+                color: "#999"
+                }
+            },
+        xaxis2: {
+            anchor: 'y2',
+            title: "AVERAGE VOLTAGE",
+            titlefont: {
+                family: "Overpass",
+                color: "#fff"
+                },
+            tickfont: {
+                family: "Overpass",
+                color: "#999"
+                },
+        },
+        xaxis: {
+            title: "AVERAGE POWER CONSUMPTION (kW)",
+            titlefont: {
+                family: "Overpass",
+                color: "#fff"
+                },
+            tickfont: {
+                family: "Overpass",
+                color: "#999"
+                },
+        },
+        yaxis: {
+            domain: [.5, 1],
+            tickfont: {
+                family: "Overpass",
+                color: "#999"
+                }
+            },
         margin: {
             t:0
+        },
+        legend: {
+            font: {color: "999"},
+            orientation: "h"
         }
     };
 
@@ -100,7 +253,10 @@ function drawGraphs(data){
             x: data[0],
             y: data[3],
             name: "Sub-component 1",
-            type: "bar"
+            type: "bar",
+            marker: {
+                color: "#5F667D"
+            }
         };
 
     var sub2graph = {
@@ -109,7 +265,10 @@ function drawGraphs(data){
             xaxis: 'x2',
             yaxis: 'y2',
             name: "Sub-component 2",
-            type: "bar"
+            type: "bar",
+            marker: {
+                color: "#6A6994"
+            }
         };
 
     var sub3graph = {
@@ -118,23 +277,61 @@ function drawGraphs(data){
             xaxis: 'x3',
             yaxis: 'y3',
             name: "Sub-component 3",
-            type: "bar"
+            type: "bar",
+            marker: {
+                color: "#697E94"
+            }
         };
 
     var subs = [sub1graph, sub2graph, sub3graph];
 
     var subsLayout = {
-      xaxis: {domain: [0, .32]},
+        showlegend: false,
+        paper_bgcolor: "#252831",
+        plot_bgcolor: "#252831",
+      xaxis: {
+        domain: [0, .32],
+        title: "SUB-COMPONENT 1 (kWh)",
+        titlefont: {
+            family: "Overpass",
+            color: "#fff"
+            },
+        tickfont: {
+            family: "Overpass",
+            color: "#999"
+            },
+        },
       yaxis2: {anchor: 'x2'},
-      xaxis2: {domain: [0.35, .66]},
+      xaxis2: {
+        domain: [0.35, .66],
+        title: "SUB-COMPONENT 2 (kWh)",
+        titlefont: {
+            family: "Overpass",
+            color: "#fff"
+            },
+        tickfont: {
+            family: "Overpass",
+            color: "#999"
+            },
+        },
       yaxis3: {anchor: 'x3'},
-      xaxis3: {domain: [0.69, 1]},
+      xaxis3: {
+        domain: [0.69, 1],
+        title: "SUB-COMPONENT 3 (kWh)",
+        titlefont: {
+            family: "Overpass",
+            color: "#fff"
+            },
+        tickfont: {
+            family: "Overpass",
+            color: "#999"
+            },
+        },
         margin: {
-            t:0
+            t:30
         }
     };
 
     Plotly.newPlot('consumption', consumption, topLayout);
     Plotly.newPlot('subs', subs, subsLayout);
-    //Plotly.newPlot('voltage', voltage);
 }
